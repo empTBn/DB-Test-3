0d3cbbe379f15929d31dad191827e0653baf7433
Matemm <emmajose1703@gmail.com>
2023-11-26 11:09:10 -0600

    login con verificacion

--- SP/sp_Login.sql
+++ SP/sp_Login.sql
@@ -1,60 +1,57 @@
-USE [Tarea3]
-GO
-/****** Object:  StoredProcedure [dbo].[sp_Login]     ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE or ALTER PROCEDURE [dbo].[sp_Login] 
-	@Usuario VARCHAR(64), --Nombre del usuario
-	@Password VARCHAR(64), --Contraseña del usuario
-	@output INT OUT	-- Codigo de resultado el cual su uso saber si el usuario entra o no
+ALTER PROCEDURE [dbo].[sp_Login] 
+    @Usuario VARCHAR(64), --Nombre del usuario
+    @Password VARCHAR(64), --Contraseña del usuario
+    @output INT OUT	-- Codigo de resultado el cual su uso saber si el usuario entra o no
 AS
 BEGIN
-	SET NOCOUNT ON;
-	
-	BEGIN TRY
+    SET NOCOUNT ON;
+    
+    BEGIN TRY
+        SET @output = 0;  -- El codigo de resultado es 0 porque solo se modifica si se rechaza el 
 
-		-- Try inicial;
-			SET @output=0;  -- El codigo de resultado es 0 porque solo se modifica si se rechaza el 
+        IF EXISTS (SELECT id FROM dbo.Empleado A WHERE A.Usuario = @Usuario AND A.Password = @Password)
+        BEGIN
+            --En caso de que el usuario y la contraseña coincidan
+            SET @output = 1;
+            INSERT INTO dbo.EventLog VALUES(
+                ('LOGIN PROCEDURE SUCCESS'),
+                (SELECT id FROM dbo.Empleado A WHERE A.Usuario = @Usuario AND A.Password = @Password),
+                (SELECT @@SERVERNAME),
+                GETDATE()
+            );
+            -- Redirect a admin o empleado basado en la verificacion
+            IF EXISTS (SELECT id FROM dbo.Usuario WHERE UserName = @Usuario)
+                SELECT 'admin.ejs' AS RedirectPage; -- Redirect a admin
+            ELSE
+                SELECT 'employee.ejs' AS RedirectPage; -- Redirect a interfaz de empleado
+        END
+        ELSE
+        BEGIN
+            INSERT INTO dbo.EventLog VALUES(
+                ('LOGIN PROCEDURE FAILED'),
+                (SELECT id FROM dbo.Empleado A WHERE A.Usuario = @Usuario AND A.Password = @Password),
+                (SELECT @@SERVERNAME),
+                GETDATE()
+            );
+            -- Return some failure status if needed
+        END
+    
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+        BEGIN
+            INSERT INTO dbo.DBErrors VALUES (
+                SUSER_SNAME(),
+                ERROR_NUMBER(),
+                ERROR_STATE(),
+                ERROR_SEVERITY(),
+                ERROR_LINE(),
+                ERROR_PROCEDURE(),
+                ERROR_MESSAGE(),
+                GETDATE()
+            );
+        END
+    END CATCH
 
-			IF EXISTS (SELECT id FROM dbo.Empleado A WHERE A.Usuario=@Usuario AND A.Password=@Password)
-			BEGIN
-			--En caso de que el usuario y la contraseña y coincidan
-			SET @output=1;
-			INSERT INTO dbo.EventLog VALUES(
-				('LOGIN PROCEDURE SUCCESS'),
-				(SELECT id FROM dbo.Empleado A WHERE A.Usuario=@Usuario AND A.Password=@Password),
-				(SELECT @@SERVERNAME),
-				GETDATE()
-		);    
-			RETURN;
-			END; 
-		INSERT INTO dbo.EventLog VALUES(
-			('LOGIN PROCEDURE FAILLED'),
-			(SELECT id FROM dbo.Empleado A WHERE A.Usuario=@Usuario AND A.Password=@Password),
-			(SELECT @@SERVERNAME),
-			GETDATE()
-		);    
-    -- Falta que determine si es admin o empleado
-	
-	END TRY
-	BEGIN CATCH
-		IF @@TRANCOUNT>0
-
-
-		INSERT INTO dbo.DBErrors VALUES (
-			SUSER_SNAME(),
-			ERROR_NUMBER(),
-			ERROR_STATE(),
-			ERROR_SEVERITY(),
-			ERROR_LINE(),
-			ERROR_PROCEDURE(),
-			ERROR_MESSAGE(),
-			GETDATE()
-		);
-
-	END CATCH
-
-	SET NOCOUNT OFF;
+    SET NOCOUNT OFF;
 END;
